074fafe99b465e9ab7f767d7b100994c21fe6982
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-09 12:57:22 +0530

    Initial Commit

---
+++ Fitness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fitness
+//
+//  Created by Mahesh Prasad on 09/08/20.
+//  Copyright © 2020 CreatesApp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
